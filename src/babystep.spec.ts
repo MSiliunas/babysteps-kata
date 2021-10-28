31283cca510dbf3862a4d022e8bf34e51bdc2fb8
MSiliunas <marijuss@wix.com>
2021-10-29 01:34:31 +0300

    JSDom

--- src/babystep.spec.ts
+++ src/babystep.spec.ts
@@ -1,5 +1,27 @@
-describe('test', () => {
-  it('passes', () => {
-    expect(true).toBeTruthy;
+import { JSDOM } from 'jsdom';
+
+describe('baby steps timer', () => {
+  let dom: JSDOM;
+
+  beforeEach(async () => {
+    dom = await JSDOM.fromFile('src/index.html', {
+      resources: 'usable',
+      runScripts: 'dangerously',
+    });
+    await wait(100);
+  });
+  afterEach(async () => {
+    dom.window.close();
   })
+
+  it('renders', async () => {
+    expect(dom.serialize()).toBe(`<!DOCTYPE html><html><head></head>
+  <body><div style=\"border: 3px solid #555555; background: #ffffff; margin: 0; padding: 0;\"><h1 style=\"text-align: center; font-size: 30px; color: #333333;\">02:00</h1><div style=\"text-align: center\"><a style=\"color: #555555;\" href=\"javascript:command('start');\">Start</a> <a style=\"color: #555555;\" href=\"javascript:command('quit');\">Quit</a> </div></div></body></html>`);
+  });
 });
+
+function wait(millis) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millis);
+  });
+}
